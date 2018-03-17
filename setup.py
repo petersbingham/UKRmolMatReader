cb101efa6867132e5a5c3fba4cbf5ac64c049974
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 11:09:16 +0000

    Copy test file and README on installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import shutil
+shutil.copy('README.md', 'UKRmolMatReader/README.md')
 
 setup(name='UKRmolMatReader',
-      version='0.4',
+      version='0.5',
       description='Python package to read K-matrix files produced by the UKRmol software.',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['UKRmolMatReader'],
+      package_data={'UKRmolMatReader': ['tests/*', 'README.md'],
      )
